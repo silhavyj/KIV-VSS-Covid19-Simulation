a0c6da5cdd3696a1f1a45753991ecdd180187231
silhavyj <jakub.sil@seznam.cz>
2022-11-26 18:14:05 +0100

    backup

--- src/simulation/mobility_model.cpp
+++ src/simulation/mobility_model.cpp
@@ -21,28 +21,38 @@ namespace kiv_vss
 
     void CMobility_Model::Update()
     {
+        // There is nothing to do if the person has died.
         if (m_person->Get_Infection_State() == CPerson::NInfection_State::Dead)
         {
             return;
         }
         if (m_person_is_moving)
         {
+            // Move the person toward their final destination.
             m_person->Move_Toward(m_next_location);
+
+            // Check if they have reached the destination.
             if (m_person->Get_Current_Location() == m_next_location)
             {
                 m_person_is_moving = false;
+
+                // Generate a random period of time to be spent at the location.
                 m_at_location_countdown = Generate_Random_Hours_At_Location();
             }
         }
         else
         {
+            // Person stays at the location.
             if (0 != m_at_location_countdown)
             {
                 --m_at_location_countdown;
             }
             else
             {
+                // Update the person infection state.
                 Check_Infection_State();
+
+                // Pick the next location.
                 m_next_location = Pick_Next_Location();
                 m_person_is_moving = true;
             }
@@ -51,14 +61,21 @@ namespace kiv_vss
 
     void CMobility_Model::Check_Infection_State()
     {
+        // Flip flop state, so we catch the person infected only once.
         static bool seen_infected = false;
+
         const auto person_is_infected = m_person->Get_Infection_State() == CPerson::NInfection_State::Infected;
 
+        // Person has been infected.
         if (!seen_infected && person_is_infected)
         {
             seen_infected = true;
+
+            // Check if the person is going to self-isolate due to the virus.
             m_self_isolating_due_to_infection = utils::Try_Event(m_config->disease.self_isolating_when_infected);
         }
+
+        // The person is no longer infected 
         if (seen_infected && !person_is_infected)
         {
             seen_infected = false;
@@ -68,6 +85,7 @@ namespace kiv_vss
 
     CLocation CMobility_Model::Pick_Next_Location() const
     {
+        // Helper lambda function - creates a list of probabilities.
         const auto Construct_Transition_Probabilities = [&](const TMobility::TTransition& transition)
         {
             std::vector<double> probabilities = {
@@ -75,10 +93,14 @@ namespace kiv_vss
                 transition.go_to_popular_location_prob,
                 transition.go_home_prob
             };
+
             return probabilities;
         };
 
+        // Probabilities of what the next location is going to be.
         std::vector<double> random_events;
+
+        // If the person is self-isolating, these probabilities differ.
         if (m_self_isolating || m_self_isolating_due_to_infection)
         {
             random_events = Construct_Transition_Probabilities(m_config->mobility.isolation);
@@ -88,38 +110,46 @@ namespace kiv_vss
             random_events = Construct_Transition_Probabilities(m_config->mobility.non_isolation);
         }
 
+        // Roll dice as to what the next location is going to be.
         const auto event = static_cast<std::uint8_t>(utils::Pick_Event(random_events));
         const auto action = static_cast<NMobility_State>(event);
         CLocation next_location{0,0};
 
         switch (action)
         {
+            // Person will go to a random location. 
             case NMobility_State::Headed_To_Random_Location:
                 next_location = CLocation::Generate_Random_In_Square_Location(0, m_config->general.world_size);
                 break;
 
+            // Person will go to one of the popular places.
             case NMobility_State::Headed_To_Popular_Place:
                 next_location = Pick_Random_Popular_Place();
                 break;
 
+            // Person will head back home.
             case NMobility_State::Headed_Home:
                 next_location = m_person->Get_Home_Location();
                 break;
 
+            // An error has ocurred.
             default:
                 spdlog::error("ERROR: Pick_Event did not return a valid NMobility_State. Returning [0; 0] as the default position.");
                 break;
         }
 
+        // Return the next location.
         return next_location;
     }
 
     inline CLocation CMobility_Model::Pick_Random_Popular_Place() const
     {
+        // If there are no popular locations, return a random location instead.
         if (m_popular_places->empty())
         {
             return CLocation::Generate_Random_In_Square_Location(0, m_config->general.world_size);
         }
+
         const std::size_t index = utils::Random<std::uniform_int_distribution<std::size_t>>(0, m_popular_places->size() - 1);
         return m_popular_places->at(index);
     }
--- src/simulation/mobility_model.h
+++ src/simulation/mobility_model.h
@@ -7,37 +7,64 @@
 
 namespace kiv_vss
 {
+    /// \author Jakub Silhavy
+    /// This class defines the mobility model that moves
+    /// the people around. It captures a simplified version
+    /// of how people travel in the real word.
     class CMobility_Model
     {
     public:
+        /// Creates an instance of the class.
+        /// \param person Person to be moved around by the mobility model
+        /// \param popular_places Popular places people tend to go
+        /// \param self_isolating Flag indicating whether or not the person is self-isolating
         explicit CMobility_Model(CPerson* person, const std::vector<CLocation>* popular_places, bool self_isolating);
+
+        /// Default destructor.
         ~CMobility_Model() = default;
 
+        /// Updates the person's state.
+        /// Each person is allowed to stay at a location for a certain amount of time.
+        /// When the time is up, this method will decided the person's next location, and
+        /// it moves them towards the location until they have reached the destination.
         void Update();
 
     private:
+        /// Checks if the user is going to self-isolate due to catching the virus.
+        /// Also, it checks whether the user can stop quarantining as they have recovered.
         void Check_Infection_State();
+
+        /// Picks the next locating the person will go.
+        /// \return Person't next final destination
         [[nodiscard]] CLocation Pick_Next_Location() const;
+
+        /// Picks a random popular location.
+        /// \return Arbitrary popular location.
         [[nodiscard]] CLocation Pick_Random_Popular_Place() const;
+
+        /// Generates a random period of time for which the user
+        /// will be allowed to stay at the next location.
+        /// \return Random period of time to spend at a location
         [[nodiscard]] std::size_t Generate_Random_Hours_At_Location() const;
 
     private:
+        /// Mobility state of a person
         enum class NMobility_State : std::uint8_t
         {
-            Headed_To_Random_Location,
-            Headed_To_Popular_Place,
-            Headed_Home
+            Headed_To_Random_Location, ///< Person is going to an arbitrary location
+            Headed_To_Popular_Place,   ///< Person is on their way to a popular location
+            Headed_Home                ///< Person is going back home
         };
 
     private:
-        CPerson* m_person;
-        const std::vector<CLocation>* m_popular_places;
-        bool m_self_isolating;
-        bool m_self_isolating_due_to_infection;
-        const TConfig* m_config;
-        bool m_person_is_moving;
-        CLocation m_next_location;
-        std::size_t m_at_location_countdown;
+        CPerson* m_person;                              ///< Person to be moved around
+        const std::vector<CLocation>* m_popular_places; ///< List of popular places people tend to go
+        bool m_self_isolating;                          ///< Flag if the person is willingly self-isolating
+        bool m_self_isolating_due_to_infection;         ///< Flag if the person is self-isolating due to infection
+        const TConfig* m_config;                        ///< Configuration of the simulation
+        bool m_person_is_moving;                        ///< Flag if the person si headed to a location
+        CLocation m_next_location;                      ///< Location the person is going to
+        std::size_t m_at_location_countdown;            ///< Countdown of how much timer the user has spent at a location
     };
 }
 
